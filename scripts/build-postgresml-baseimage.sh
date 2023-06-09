831bd04857aa22f961da41948a36f5b7052a2ef4
Tola Awofolu <oawofolu@pivotal.io>
2023-06-09 13:37:44 -0400

    Updated ARG/ENV values

--- scripts/build-postgresml-baseimage.sh
+++ scripts/build-postgresml-baseimage.sh
@@ -3,8 +3,8 @@ source .env
 echo -e ${DATA_E2E_LLM_REGISTRY_PASSWORD} | docker login -u ${DATA_E2E_LLM_REGISTRY_USERNAME} --password-stdin
 
 docker build -t \
-    --build-arg IMAGE_TAG=${DATA_E2E_POSTGRESML_IMAGE_TAG} \
-    --build-arg PG_VS=${DATA_E2E_POSTGRESML_VERSION} \
+    --build-arg IMAGE_TAG_VAL=${DATA_E2E_POSTGRESML_IMAGE_TAG} \
+    --build-arg PG_VS_VAL=${DATA_E2E_POSTGRESML_VERSION} \
     ${DATA_E2E_LLM_REGISTRY_USERNAME}/postgresml-bitnami:${DATA_E2E_POSTGRESML_IMAGE_TAG} .
 
 docker push \
